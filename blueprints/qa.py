9b2d9e22405c6d1e5b3ac2a2b5499889f878af77
lgxssssss <127862134+lgxssssss@users.noreply.github.com>
2024-08-18 21:38:23 +0800

    发布问答静态布局

--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,6 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 bp = Blueprint("qa", __name__, url_prefix='/')
 @bp.route("/")
 def login():
-    return "我他妈来了"
+    return "欢迎来到首页"
+
+@bp.route("/qa/public", methods=['GET', 'POST'])
+def qa():
+    if request .method == 'GET':
+        return render_template("public_question.html")
